78bd8d23be63b134342f6f0e23177b52cfa69ea7
dfa1234 <dfaure@outlook.com>
2021-12-30 11:03:15 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import './style.css';
 
 const inputMyCheck = document.getElementById('mycheck');
-inputMyCheck.addEventListener('change', function () {
-  console.log(this.value);
+console.log('mycheck is', inputMyCheck.checked);
 
-  if (this.value === 'on') {
-    this.value = 'off';
-  } else {
-    this.value = 'on';
-  }
+inputMyCheck.addEventListener('change', function () {
+  console.log('mycheck is', this.checked);
+});
+
+const inputMyCheck2 = document.getElementById('mycheck2');
+console.log('mycheck2 is', inputMyCheck2.checked);
+
+inputMyCheck2.addEventListener('change', function () {
+  console.log('mycheck2 is', this.checked);
+});
+
+const inputMyCheck3 = document.getElementById('mycheck3');
+console.log('mycheck3 is', inputMyCheck3.checked);
+
+inputMyCheck3.addEventListener('change', function () {
+  console.log('mycheck3 is', this.checked);
 });
