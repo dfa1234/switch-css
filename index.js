8d56aea03509a5b61d83c118aadaa3681c581a03
dfa1234 <dfaure@outlook.com>
2022-02-22 20:36:55 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 import './style.css';
 
-const inputMyCheck = document.getElementById('mycheck');
-console.log('mycheck is', inputMyCheck.checked);
+const inputMyCheck1 = document.getElementById('mycheck1');
+console.log('Start: mycheck1 is', inputMyCheck1.checked);
 
-inputMyCheck.addEventListener('change', function () {
-  console.log('mycheck is', this.checked);
+inputMyCheck1.addEventListener('change', function () {
+  console.log('mycheck1 is now', this.checked);
 });
 
 const inputMyCheck2 = document.getElementById('mycheck2');
-console.log('mycheck2 is', inputMyCheck2.checked);
+console.log('Start: mycheck2 is', inputMyCheck2.checked);
 
 inputMyCheck2.addEventListener('change', function () {
-  console.log('mycheck2 is', this.checked);
+  console.log('mycheck2 is now', this.checked);
 });
-
